d6aca5dd0b0cf6f36f11dedf6afcd591a30ef8dd
blockofone <>
2021-12-16 19:12:02 +0800

    add getResultByTrxHash function for groupResult.

--- core/parallel/groupResult.go
+++ core/parallel/groupResult.go
@@ -53,6 +53,38 @@ type StorageRecord struct {
 	Value common.Hash
 }
 
+func NewTrxResult(receipt *types.Receipt, logs []*types.Log, touchedAddresses *TouchedAddressObject, usedGas uint64) *TrxResult {
+	return &TrxResult{receipt: receipt, logs: logs, touchedAddresses: touchedAddresses, usedGas: usedGas}
+}
+
+func NewGroupResult() *GroupResult {
+	return &GroupResult{}
+}
+
+func NewAccountRecord(index int, balance *big.Int, codeHash []byte, nonce uint64) *AccountRecord {
+	return &AccountRecord{index: index, balance: balance, codeHash: codeHash, nonce: nonce}
+}
+
+func NewStorageRecord(index int, value common.Hash) *StorageRecord {
+	return &StorageRecord{index: index, Value: value}
+}
+
+func (gr *GroupResult) merge(result *GroupResult) {
+	for addr, records := range result.accountRecords {
+		gr.accountRecords[addr] = append(gr.accountRecords[addr], records...)
+	}
+	for storage, records := range result.storageRecords {
+		gr.storageRecords[storage] = append(gr.storageRecords[storage], records...)
+	}
+	//for k, v := range result.trxHashToResult {
+	//	gr.trxHashToResult[k] = v
+	//}
+	for k, v := range result.trxIndexToResult {
+		gr.trxIndexToResult[k] = v
+	}
+	//gr.errs = append(gr.errs, result.errs...)
+}
+
 func (gr *GroupResult) removeResultAfterTrxPos(index int) {
 	for _, records := range gr.accountRecords {
 		for i := len(records) - 1; i > 0; i-- {
@@ -75,28 +107,74 @@ func (gr *GroupResult) removeResultAfterTrxPos(index int) {
 	}
 }
 
-func (gr *GroupResult) rollbackExecResult(trxHashes map[common.Hash]struct{}) *TouchedAddressObject {
-	totalTouchedAddress := NewTouchedAddressObject()
+//func (gr *GroupResult) rollbackExecResult(trxHashes map[common.Hash]struct{}) *TouchedAddressObject {
+//	totalTouchedAddress := NewTouchedAddressObject()
+//
+//	for trxHash, _ := range trxHashes {
+//		touchedAddressObj := gr.trxHashToTouchedAddress[trxHash]
+//		delete(gr.trxHashToTouchedAddress, trxHash)
+//		totalTouchedAddress.Merge(touchedAddressObj)
+//
+//		for account, op := range touchedAddressObj.AccountOp() {
+//			if op {
+//				delete(gr.accountRecords, account)
+//			}
+//		}
+//
+//		for storage, op := range touchedAddressObj.StorageOp() {
+//			if op {
+//				delete(gr.storageRecords, storage)
+//			}
+//		}
+//
+//		delete(gr.receipt, trxHash)
+//	}
+//
+//	return totalTouchedAddress
+//}
 
-	for trxHash, _ := range trxHashes {
-		touchedAddressObj := gr.trxHashToTouchedAddress[trxHash]
-		delete(gr.trxHashToTouchedAddress, trxHash)
-		totalTouchedAddress.Merge(touchedAddressObj)
+func (gr* GroupResult) getResultByTrxHash(trxHash common.Hash) {
 
-		for account, op := range touchedAddressObj.AccountOp() {
-			if op {
-				delete(gr.accountRecords, account)
-			}
-		}
+	groupResult := NewGroupResult()
+	touchedAddressObj := gr.trxHashToTouchedAddress[trxHash]
 
-		for storage, op := range touchedAddressObj.StorageOp() {
-			if op {
-				delete(gr.storageRecords, storage)
-			}
-		}
-
-		delete(gr.receipt, trxHash)
+	for addr, op := range touchedAddressObj.accountOp {
+		if op {
+			if accountRecords, ok := gr.accountRecords[addr]; ok {
+				groupResult.accountRecords[addr] = accountRecords
+		   }
+	   }
 	}
 
-	return totalTouchedAddress
+   for storage, op := range touchedAddressObj.storageOp {
+	   if op {
+		   if storageRecords, ok := gr.storageRecords[storage]; ok {
+			   groupResult.storageRecords[storage] = storageRecords
+		   }
+	   }
+   }
+
+   for (Map.Entry<String, Boolean> entry : touchedAddressObjMap.get(trxHash).getTouchedAccounts().entrySet()) {
+	   if (entry.getValue()) {
+		   String address = entry.getKey();
+		   List<AccountRecordObj> accountRecordObjs = this.accountRecords.get(address);
+		   if (accountRecordObjs != null) {
+			   groupExecResult.getAccountRecords().put(address, accountRecordObjs);
+			   if (accountRecordObjs.get(0).isContract() && contractAccountStates.containsKey(address)) {
+				   groupExecResult.getContractAccountStates().put(address, this.contractAccountStates.get(address));
+			   }
+		   }
+   
+		   ContractObj contractObj = this.contractObjs.get(address);
+		   if (contractObj != null) {
+			   groupExecResult.getContractObjs().put(address, contractObj);
+		   }
+	   }
+   }
+   
+   groupExecResult.getExecResults().put(trxHash, this.execResults.get(trxHash));
+   groupExecResult.getFee4Miner().put(trxHash, this.fee4Miner.get(trxHash));
+   groupExecResult.getTouchedAddressObjMap().put(trxHash, this.touchedAddressObjMap.get(trxHash));
+   
+   return groupExecResult;
 }
