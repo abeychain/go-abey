522ca9f99cb08f859c8501cdf47378ef23c96c84
blockofone <>
2022-04-15 11:40:06 +0800

    add test01 for a lot of txs execute

--- send_transaction/contract/parallel_test.go
+++ send_transaction/contract/parallel_test.go
@@ -24,7 +24,7 @@ func init() {
 }
 
 func DefaulGenesisBlock() *core.Genesis {
-	i, _ := new(big.Int).SetString("90000000000000000000000", 10)
+	i, _ := new(big.Int).SetString("90000000000000000000000000", 10)
 	key1 := hexutil.MustDecode("0x04d341c94a16b02cee86a627d0f6bc6e814741af4cab5065637aa013c9a7d9f26051bb6546030cd67e440d6df741cb65debaaba6c0835579f88a282193795ed369")
 	key2 := hexutil.MustDecode("0x0496e0f18d4bf38e0b0de161edd2aa168adaf6842706e5ebf31e1d46cb79fe7b720c750a9e7a3e1a528482b0da723b5dfae739379e555a2893e8693747559f83cd")
 	key3 := hexutil.MustDecode("0x0418196ee090081bdec01e8840941b9f6a141a713dd3461b78825edf0d8a7f8cdf3f612832dc9d94249c10c72629ea59fbe0bdd09bea872ddab2799748964c93a8")
@@ -130,9 +130,70 @@ func TestParallelTX(t *testing.T) {
 	//	"insertblock", common.PrettyDuration(time.Duration(int64(params.InsertBlockTime)/repeat)))
 }
 
-func abeyToWei(trueValue uint64) *big.Int {
+func Test01(t *testing.T) {
+	params.MinTimeGap = big.NewInt(0)
+	params.SnailRewardInterval = big.NewInt(3)
+
+	sendNumber := 100
+	delegateKey := make([]*ecdsa.PrivateKey, sendNumber)
+	delegateAddr := make([]common.Address, sendNumber)
+	for i := 0; i < sendNumber; i++ {
+		delegateKey[i], _ = crypto.GenerateKey()
+		delegateAddr[i] = crypto.PubkeyToAddress(delegateKey[i].PublicKey)
+	}
+	genesis := gspec.MustFastCommit(db)
+	chain, _ := core.GenerateChain(gspec.Config, genesis, engine, db, 10, func(i int, gen *core.BlockGen) {
+		switch i {
+		case 0:
+			// In block 1, addr1 sends addr2 some ether.
+			fmt.Println("balance ", weiToAbey(gen.GetState().GetBalance(mAccount)))
+			nonce := gen.TxNonce(mAccount)
+			for _, v := range delegateAddr {
+				tx, _ := types.SignTx(types.NewTransaction(nonce, v, abeyToWei(20), params.TxGas, nil, nil), signer, priKey)
+				gen.AddTx(tx)
+				nonce = nonce + 1
+			}
+		case 1:
+			// In block 2, addr1 sends some more ether to addr2.
+			//nonce := gen.TxNonce(mAccount)
+			//key, _ := crypto.GenerateKey()
+			//coinbase := crypto.PubkeyToAddress(key.PublicKey)
+			//for i := 0; i < sendNumber; i++ {
+			//	tx, _ := types.SignTx(types.NewTransaction(nonce, coinbase, abeyToWei(2), params.TxGas, nil, nil), signer, priKey)
+			//	gen.AddTx(tx)
+			//	nonce = nonce + 1
+			//}
+		case 3:
+			for k, v := range delegateAddr {
+				key, _ := crypto.GenerateKey()
+				coinbase := crypto.PubkeyToAddress(key.PublicKey)
+				nonce := gen.TxNonce(v)
+				for i := 0; i < 1; i++ {
+					tx, _ := types.SignTx(types.NewTransaction(nonce, coinbase, new(big.Int).SetInt64(30000), params.TxGas, nil, nil), signer, delegateKey[k])
+					gen.AddTx(tx)
+					nonce = nonce + 1
+				}
+			}
+		}
+	})
+
+	repeat := int64(2)
+	for i := 0; i < int(repeat); i++ {
+		db1 := abeydb.NewMemDatabase()
+		gspec.MustFastCommit(db1)
+
+		blockchain, err := core.NewBlockChain(db1, nil, gspec.Config, engine, vm.Config{})
+		if err != nil {
+			fmt.Println("NewBlockChain ", err)
+		}
+		if _, err := blockchain.InsertChain(chain); err != nil {
+			panic(err)
+		}
+	}
+}
+func abeyToWei(vaule uint64) *big.Int {
 	baseUnit := new(big.Int).Exp(big.NewInt(10), big.NewInt(18), nil)
-	value := new(big.Int).Mul(big.NewInt(int64(trueValue)), baseUnit)
+	value := new(big.Int).Mul(big.NewInt(int64(vaule)), baseUnit)
 	return value
 }
 
